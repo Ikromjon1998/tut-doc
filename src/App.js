72ef9057ed13c258b9569db0b2f907913fd798f0
Ikromjon <ikromjon98.98@icloud.com>
2022-08-19 01:52:36 +0200

    added Conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
-import React from 'react';
-import Welcome from './Welcome';
-import Comment from './Comment';
+import Toggle from './Toggle';
 import Clock from './Clock';
+import Greeting from './Greeting'
+import LoginControl from './LoginControl';
+import Mailbox from './Mailbox';
+import Page from './Page';
 
 
 
 function App() {
+
+  const messages = ['React', 'Re: React', 'Re:Re: React'];
   return (
     <div>
+      <Mailbox unreadMessages={messages} />
+      <Page />
+      <Toggle />
+      <Greeting isLoggedIn={true} />
       <Clock />
       <Clock />
       <Clock />
+      <LoginControl />
     </div>
   );
 }
